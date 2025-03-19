1a8a2378f46a12d9735796714b2dafdc71ffa341
lucasA49 <lucasaksu@gmail.com>
2025-03-19 17:57:01 +0100

    mise en place du back 1/2

--- src/Index.js
+++ src/Index.js
@@ -4,9 +4,13 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send("Ma reponse test !")
 });
+
+app.get("/")
+
+
 app.listen(3000, () => {
     console.log('Server is running on URL httjp://localhost:${PORT}');
 });
